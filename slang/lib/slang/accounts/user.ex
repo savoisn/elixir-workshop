b4f4b1a52d7dfe9ceb74b3d8061dd56f46eabc58
Nicolas SAVOIS <nicolas.savois@talan.com>
2019-01-21 00:00:39 +0100

    hashing password

--- slang/lib/slang/accounts/user.ex
+++ slang/lib/slang/accounts/user.ex
@@ -2,6 +2,7 @@ defmodule Slang.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Slang.Accounts.User
 
   schema "users" do
     field :email, :string
@@ -13,9 +14,26 @@ defmodule Slang.Accounts.User do
   end
 
   @doc false
-  def changeset(user, attrs) do
+  def changeset(%User{} = user, attrs) do
     user
     |> cast(attrs, [:email, :password, :password_confirmation])
     |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/) # Check that email is valid
+    |> validate_length(:password, min: 8) # Check that password length is >= 8
+    |> validate_confirmation(:password) # Check that password === password_confirmation
+    |> unique_constraint(:email)
+    |> put_password_hash # Add put_password_hash to changeset pipeline
   end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+          changeset
+    end
+  end
+
+
 end
